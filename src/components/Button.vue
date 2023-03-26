eb74f13d320170b6935271815c0d865d5afdab0b
Ignatius Tan <tanignatiustan@gmail.com>
2023-03-27 01:47:20 +0800

    Added landing page

---
+++ src/components/Button.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="button-container" v-bind:class="rootClassName">
+      <button class="button-button button">{{ button }}</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Button',
+    props: {
+      button: {
+        type: String,
+        default: 'Button',
+      },
+      rootClassName: String,
+    },
+  }
+  </script>
+  
+  <style scoped>
+  .button-container {
+    width: 169px;
+    height: 44px;
+    display: flex;
+    position: relative;
+    align-items: flex-start;
+    flex-direction: column;
+  }
+  .button-button {
+    width: 100%;
+    height: 100%;
+    box-shadow: 5px 5px 10px 0px #d4d4d4;
+    margin-top: var(--dl-space-space-halfunit);
+    text-align: center;
+    margin-bottom: var(--dl-space-space-halfunit);
+    background-color: #ADE2DF;
+  }
+  
+  </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,334 @@
-
 <template>
-  <main class="home">
-    <h1>Home</h1>
-  </main>
+  <div class="home-container">
+    <div class="home-header">
+      <app-navigation rootClassName="navigation-root-class-name"></app-navigation>
+      <div class="home-hero">
+        <div class="home-container1">
+          <h1 class="home-text">PillPal</h1>
+          <h2 class="home-title">
+            PillPal is the perfect solution for those who need an extra reminder
+            to take their medication! Our easy-to-use web application functions as
+            an integrated healthcare application for use by healthcare
+            professionals and patients alike.
+          </h2>
+          <app-button
+            button="Get Started"
+            rootClassName="button-root-class-name"
+          ></app-button>
+        </div>
+        <div class="home-images"></div>
+      </div>
+    </div>
+    <div class="home-container2">
+      <h1 class="home-text01">Features</h1>
+      <section class="home-features">
+        <div class="home-feature-card">
+          <svg viewBox="0 0 1024 1024" class="home-icon">
+            <path
+              d="M814 210q124 124 124 302 0 176-125 301t-301 125-301-125-125-301 125-301 301-125h42v352q44 24 44 74 0 34-26 60t-60 26-60-26-26-60q0-50 44-74v-90q-56 16-92 60t-36 104q0 70 50 120t120 50 120-50 50-120q0-66-50-120l60-60q76 76 76 180 0 106-75 181t-181 75-181-75-75-181q0-94 61-165t153-87v-86q-126 16-213 112t-87 226q0 140 101 241t241 101 241-101 101-241q0-58-29-129t-71-113z"
+            ></path>
+          </svg>
+          <h2 class="home-text02">Track</h2>
+          <span class="home-text03">
+            Set alarms to take your medication on time
+          </span>
+        </div>
+        <div class="home-feature-card1">
+          <svg viewBox="0 0 1024 1024" class="home-icon02">
+            <path
+              d="M896 170.667h-768c-47.104 0-85.333 38.229-85.333 85.333v512c0 47.104 38.229 85.333 85.333 85.333h768c47.104 0 85.333-38.229 85.333-85.333v-512c0-47.104-38.229-85.333-85.333-85.333zM128 256h341.333v512h-341.333v-512zM896 768h-384v-512h384.128l-0.128 512z"
+            ></path>
+            <path
+              d="M853.333 576c0-11.733-9.6-21.333-21.333-21.333h-42.667c-11.733 0-21.333 9.6-21.333 21.333v128c0 11.733 9.6 21.333 21.333 21.333h42.667c11.733 0 21.333-9.6 21.333-21.333v-128z"
+            ></path>
+            <path
+              d="M725.333 320c0-11.733-9.6-21.333-21.333-21.333h-128c-11.733 0-21.333 9.6-21.333 21.333v213.333c0 11.733 9.6 21.333 21.333 21.333h128c11.733 0 21.333-9.6 21.333-21.333v-213.333z"
+            ></path>
+            <path
+              d="M789.333 426.667h42.667c11.733 0 21.333-9.6 21.333-21.333s-9.6-21.333-21.333-21.333h-42.667c-11.733 0-21.333 9.6-21.333 21.333s9.6 21.333 21.333 21.333z"
+            ></path>
+            <path
+              d="M789.333 512h42.667c11.733 0 21.333-9.6 21.333-21.333s-9.6-21.333-21.333-21.333h-42.667c-11.733 0-21.333 9.6-21.333 21.333s9.6 21.333 21.333 21.333z"
+            ></path>
+            <path
+              d="M576 640h128c11.733 0 21.333-9.6 21.333-21.333s-9.6-21.333-21.333-21.333h-128c-11.733 0-21.333 9.6-21.333 21.333s9.6 21.333 21.333 21.333z"
+            ></path>
+            <path
+              d="M704 682.667h-128c-11.733 0-21.333 9.6-21.333 21.333s9.6 21.333 21.333 21.333h128c11.733 0 21.333-9.6 21.333-21.333s-9.6-21.333-21.333-21.333z"
+            ></path>
+            <path
+              d="M789.333 341.333h42.667c11.733 0 21.333-9.6 21.333-21.333s-9.6-21.333-21.333-21.333h-42.667c-11.733 0-21.333 9.6-21.333 21.333s9.6 21.333 21.333 21.333z"
+            ></path>
+            <path
+              d="M426.667 320c0-11.733-9.6-21.333-21.333-21.333h-213.333c-11.733 0-21.333 9.6-21.333 21.333v128c0 11.733 9.6 21.333 21.333 21.333h213.333c11.733 0 21.333-9.6 21.333-21.333v-128z"
+            ></path>
+            <path
+              d="M405.376 597.333h-213.333c-11.691 0-21.333 9.6-21.333 21.333s9.643 21.333 21.333 21.333h213.333c11.691 0 21.291-9.6 21.291-21.333s-9.6-21.333-21.291-21.333z"
+            ></path>
+            <path
+              d="M405.376 512h-213.333c-11.691 0-21.333 9.6-21.333 21.333s9.643 21.333 21.333 21.333h213.333c11.691 0 21.291-9.6 21.291-21.333s-9.6-21.333-21.291-21.333z"
+            ></path>
+            <path
+              d="M405.376 682.667h-213.333c-11.691 0-21.333 9.6-21.333 21.333s9.643 21.333 21.333 21.333h213.333c11.691 0 21.291-9.6 21.291-21.333s-9.6-21.333-21.291-21.333z"
+            ></path>
+          </svg>
+          <h2 class="home-text04">Recommendations</h2>
+          <span class="home-text05">
+            Find out what screenings suit your profile
+          </span>
+        </div>
+        <div class="home-feature-card2">
+          <div class="home-container3">
+            <svg viewBox="0 0 1024 1024" class="home-icon15">
+              <path
+                d="M469.333 554.667v341.333h-256v-341.333zM554.667 896v-341.333h256v341.333zM320 256c-17.664 0-33.621-7.125-45.269-18.731s-18.731-27.605-18.731-45.269 7.125-33.621 18.731-45.269 27.605-18.731 45.269-18.731c26.837 0 48.725 9.515 67.584 25.003 16.043 13.141 29.909 30.677 41.643 50.219 10.283 17.109 18.475 35.029 24.747 51.328zM569.429 256c6.869-17.749 15.061-35.669 25.301-52.779 11.733-19.584 25.643-37.077 41.643-50.219 18.901-15.488 40.789-25.003 67.627-25.003 17.664 0 33.621 7.125 45.269 18.731s18.731 27.605 18.731 45.269-7.125 33.621-18.731 45.269-27.605 18.731-45.269 18.731zM469.333 341.333v128h-341.333v-128h192zM838.955 256c9.216-19.413 14.379-41.088 14.379-64 0-41.216-16.768-78.635-43.733-105.6s-64.384-43.733-105.6-43.733c-49.493 0-89.984 18.347-121.685 44.331-25.728 21.077-45.568 47.061-60.715 72.277-3.413 5.675-6.613 11.349-9.6 16.981-2.987-5.632-6.187-11.307-9.6-16.981-15.147-25.216-34.987-51.2-60.715-72.277-31.701-25.984-72.192-44.331-121.685-44.331-41.216 0-78.635 16.768-105.6 43.733s-43.733 64.384-43.733 105.6c0 22.912 5.163 44.587 14.379 64h-99.712c-23.552 0-42.667 19.115-42.667 42.667v213.333c0 23.552 19.115 42.667 42.667 42.667h42.667v384c0 23.552 19.115 42.667 42.667 42.667h682.667c23.552 0 42.667-19.115 42.667-42.667v-384h42.667c23.552 0 42.667-19.115 42.667-42.667v-213.333c0-23.552-19.115-42.667-42.667-42.667zM554.667 341.333h341.333v128h-341.333z"
+              ></path>
+            </svg>
+          </div>
+          <h2 class="home-text06">
+            <span>Rewards</span>
+            <br />
+          </h2>
+          <span class="home-text09">
+            Redeem rewards when you punctually take your medications
+          </span>
+        </div>
+        <div class="home-feature-card3">
+          <svg viewBox="0 0 1170.2857142857142 1024" class="home-icon17">
+            <path
+              d="M1170.286 877.714v73.143h-1170.286v-877.714h73.143v804.571h1097.143zM950.857 292.571l146.286 512h-950.857v-329.143l256-329.143 329.143 329.143z"
+            ></path>
+          </svg>
+          <h2 class="home-text10">Analytics</h2>
+          <span class="home-text11">
+            View your medication consumption trends and more
+          </span>
+        </div>
+      </section>
+    </div>
+    <app-footer rootClassName="footer-root-class-name"></app-footer>
+  </div>
 </template>
+
+<script>
+import AppNavigation from '../components/Navigation.vue'
+import AppButton from '../components/Button.vue'
+import AppFooter from '../components/Footer.vue'
+
+export default {
+  name: 'Home',
+  props: {},
+  components: {
+    AppNavigation,
+    AppButton,
+    AppFooter,
+  },
+}
+</script>
+
+<style scoped>
+
+.home-container {
+  width: 100%;
+  display: flex;
+  min-height: 100vh;
+  overflow-x: hidden;
+  align-items: center;
+  flex-direction: column;
+}
+.home-header {
+  width: 100%;
+  height: 100vh;
+  overflow-x:hidden;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.home-hero {
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: relative;
+  max-width: 100%;
+  align-items: center;
+  flex-direction: row;
+  background-size: cover;
+  background-image: url("http://public.safetybase.co.nz/wp-content/uploads/2013/07/iStock-512489412.jpg");
+}
+.home-container1 {
+  flex: 0 0 auto;
+  width: 1128px;
+  height: 395px;
+  display: flex;
+  align-items: space-between;
+  padding-left: var(--dl-space-space-threeunits);
+  flex-direction: column;
+}
+.home-text {
+  color: #ffffff;
+  font-size: 3em;
+  margin-bottom: var(--dl-space-space-oneandhalfunits);
+}
+.home-title {
+  color: rgb(255, 255, 255);
+  width: 792px;
+  font-size: 2em;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 50px;
+}
+.home-images {
+  gap: var(--dl-space-space-oneandhalfunits);
+  margin: auto;
+  display: flex;
+  overflow: hidden;
+  position: absolute;
+  transform: rotate(-30deg);
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+}
+.home-container2 {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.home-text01 {
+  text-align: center;
+  margin-left: 25%;
+  margin-right: 25%;
+}
+.home-features {
+  width: 100%;
+  height: 332px;
+  display: flex;
+  position: relative;
+  flex-wrap: wrap;
+  max-width: 100%;
+  align-items: flex-start;
+  padding-left: var(--dl-space-space-tripleunit);
+  padding-right: var(--dl-space-space-tripleunit);
+  flex-direction: row;
+  justify-content: space-between;
+}
+.home-feature-card {
+  width: 273px;
+  height: 281px;
+  display: flex;
+  padding: 32px;
+  margin-top: var(--dl-space-space-oneandhalfunits);
+  align-items: center;
+  padding-top: 32px;
+  flex-direction: column;
+}
+.home-icon {
+  width: 50px;
+  height: 50px;
+}
+.home-text02 {
+  font-style: normal;
+  margin-top: var(--dl-space-space-unit);
+  text-align: center;
+  font-weight: 500;
+  margin-bottom: var(--dl-space-space-unit);
+}
+.home-text03 {
+  color: var(--dl-color-gray-700);
+  text-align: center;
+}
+.home-feature-card1 {
+  width: 273px;
+  height: 281px;
+  margin: var(--dl-space-space-oneandhalfunits);
+  display: flex;
+  padding: 32px;
+  margin-top: 0px;
+  align-items: center;
+  margin-left: 0px;
+  padding-top: 32px;
+  margin-right: 0px;
+  margin-bottom: 0px;
+  flex-direction: column;
+}
+.home-icon02 {
+  width: 50px;
+  height: 50px;
+}
+.home-text04 {
+  font-style: normal;
+  margin-top: var(--dl-space-space-unit);
+  text-align: center;
+  font-weight: 500;
+  margin-bottom: var(--dl-space-space-unit);
+}
+.home-text05 {
+  color: var(--dl-color-gray-700);
+  text-align: center;
+}
+.home-feature-card2 {
+  width: 273px;
+  height: 281px;
+  display: flex;
+  padding: 32px;
+  margin-top: var(--dl-space-space-oneandhalfunits);
+  align-items: center;
+  margin-left: 0px;
+  padding-top: 32px;
+  margin-right: 0px;
+  flex-direction: column;
+}
+.home-container3 {
+  flex: 0 0 auto;
+  width: auto;
+  height: 50px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+}
+.home-icon15 {
+  width: 50px;
+  height: 50px;
+}
+.home-text06 {
+  font-style: normal;
+  margin-top: var(--dl-space-space-unit);
+  text-align: center;
+  font-weight: 500;
+  margin-bottom: var(--dl-space-space-unit);
+}
+.home-text09 {
+  color: var(--dl-color-gray-700);
+  text-align: center;
+}
+.home-feature-card3 {
+  width: 267px;
+  height: 272px;
+  display: flex;
+  padding: 32px;
+  align-self: flex-start;
+  margin-top: var(--dl-space-space-oneandhalfunits);
+  align-items: center;
+  margin-left: 0px;
+  margin-right: 0px;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.home-icon17 {
+  width: 50px;
+  height: 50px;
+}
+.home-text10 {
+  font-style: normal;
+  margin-top: var(--dl-space-space-unit);
+  text-align: center;
+  font-weight: 500;
+  margin-bottom: var(--dl-space-space-unit);
+}
+.home-text11 {
+  color: var(--dl-color-gray-700);
+  text-align: center;
+}
+</style>
