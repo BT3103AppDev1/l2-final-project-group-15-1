80b74c9255248ce6a53686a285598985ccf229f9
dapscc <davidarthurp2000@gmail.com>
2023-04-11 02:23:38 +0800

    Navbar and header components, editted functions

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,23 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div id = "header" v-if="showNav()">
-    <h1 id = "home_header" v-on:click="goHome()">PillPal</h1>
-  </div>
-  <div id ="nav">
-    <div class="routing" v-if="showNav()">
-      <router-link to="/configure">Configure Medications/Supplements</router-link>
-      <router-link to="/overview">Full Medication/Supplement Overview</router-link>
-      <router-link to="/rewards">Reward Points</router-link>
-      <router-link to="/screening">Screening Recommendations</router-link>
-      <router-link to="/analytics">Patient Analytics</router-link>
-    </div>
-  <router-view/>
+  <div>
+    <Header></Header>
+    <NavBar></NavBar>
   </div>
 </template>
 
 <script>
 
-import UpdateParticulars from './components/UpdateParticulars.vue';
+import Header from './components/Header.vue';
+import NavBar from './components/NavBar.vue';
 
 export default {
   name: 'app',
-  methods: {
-    showNav: function() {
-      return this.$route.path !== '/' && this.$route.path !== "/login" && this.$route.path !== "/register" && this.$route.path !== "/forgot-password" && this.$route.path !== "/admin"
-    },
-    goHome() {
-            this.$router.push('/home')
-        }
-  }
+  components: {
+    Header,
+    NavBar,
+}
 };
 
 </script>
@@ -53,48 +41,6 @@ body, html {
   padding:0;
   width: 100%;
 }
-
-// #header{
-//   background-color: rgb(5, 214, 5);
-//   padding: 10px;
-//   color: white;
-// }
-
-// #home_header{
-//   cursor: pointer;
-//   padding: 10px;
-// }
-
-// .routing{
-//   padding: 20px;
-//   width:100%;
-// }
-
-// #nav{
-//   padding : 10px;
-// }
-
-// #nav a {
-//   font-weight : bold;
-//   text-align: center;
-//   color: #2c3e50;
-//   background-color: rgb(169, 248, 169);
-//   margin: 20px;
-//   border-radius: 2px;
-//   border: 1px;
-//   border-color: black;
-//   border-style: solid;
-//   padding: 5px;
-//   display: inline-block;
-// }
-
-// #nav a.router-link-exact-active{
-//   color: white;
-// }
-
-// h1{
-//   padding-top: 10px;
-// }
 </style>
 
 
