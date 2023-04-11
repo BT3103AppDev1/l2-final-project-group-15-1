d125fbadcce698f2477553de6a28eb1fd4e709f4
Noel-Lee <79415498+Noel-Lee@users.noreply.github.com>
2023-04-12 05:37:20 +0800

    Added statistics for medication consumtpion

--- src/views/PatientAnalyticsPage.vue
+++ src/views/PatientAnalyticsPage.vue
@@ -1,10 +1,12 @@
 <script>
   import TakeMedicationHeatMap from '../components/TakeMedicationHeatMap.vue'
+  import LateMedicationConsumption from '../components/LateMedicationConsumption.vue'
   
   export default {
     name: 'PatientAnalyticsPage',
     components: {
       TakeMedicationHeatMap,
+      LateMedicationConsumption,
      
     }
     // your ReminderView component logic here
@@ -15,6 +17,7 @@
     <div>
       <h1>Patient Analytics</h1>
       <TakeMedicationHeatMap />
+      <LateMedicationConsumption />
       
     </div>
 </template>
