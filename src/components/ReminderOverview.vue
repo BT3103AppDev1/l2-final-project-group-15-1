b1e0353adc3149206162a9667779add965c9ce29
Noel-Lee <79415498+Noel-Lee@users.noreply.github.com>
2023-04-16 10:16:21 +0800

    Created timed alert and rewards increment

--- src/components/ReminderOverview.vue
+++ src/components/ReminderOverview.vue
@@ -32,7 +32,7 @@
 
 <script>
 import firebaseApp from '../firebase.js';
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, increment, updateDoc } from "firebase/firestore";
 import { collection, getDocs, getDoc, doc, deleteDoc, query, where, setDoc } from "firebase/firestore";
 import { Timestamp } from 'firebase/firestore'
 import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -55,7 +55,112 @@ export default {
     },
     mounted() {
 
-        async function readMedication(userId) {
+        
+
+        async function setTimedAlert(userId) {
+
+          let allDocuments = await getDocs(collection(db, "PillPal", userId, "MedicationRegime"))
+
+          let index = 1
+
+          var dict = {"delay" :  parseInt(0x7fffffff), "message" : ""}
+
+
+          if (!allDocuments.empty) {
+
+              // console.log(allDocuments)
+
+            
+
+              allDocuments.forEach((docs) => {
+
+                  let row = table.insertRow(index)
+
+                  let documentData = docs.data()
+                  
+                  let medication = (documentData.Medication)
+                  let dosage = (documentData.Dosage)
+                  let frequency = (documentData.Frequency)
+                  let reminders = (documentData.Reminders)    
+                  let taken = (documentData.Taken)   
+                  let lag = (documentData.Lag)
+
+
+                  var numberOfDosesTaken = taken.length
+
+                  var minTimeBetweenDoses = Number.MAX_SAFE_INTEGER
+
+                  var timeFromLastDose = null;
+
+                  if (numberOfDosesTaken > 0) {
+                      
+                      var lastTaken = taken[taken.length - 1].toDate()
+
+                      var today = new Date();
+                      var timeFromLastDose = (today.getHours() - lastTaken.getHours()) * 60 * 60 + (today.getMinutes() - lastTaken.getMinutes()) * 60 + (today.getSeconds() - lastTaken.getSeconds())
+
+                      var hoursBetweenDoses = parseInt(reminders.split(":")[0])
+                      var minutesBetweenDoses = parseInt(reminders.split(":")[1])
+
+                      var timeBetweenDoses = hoursBetweenDoses * 60 * 60 + minutesBetweenDoses * 60
+
+                      
+                      if (timeBetweenDoses < minTimeBetweenDoses) {
+                          minTimeBetweenDoses = timeBetweenDoses
+                      }
+                      
+
+                      if (timeFromLastDose < minTimeBetweenDoses) {
+                          var timeTillNextDose = (minTimeBetweenDoses - timeFromLastDose) * 1000
+                          if (timeTillNextDose < dict["delay"]) {
+                              dict["delay"] = timeTillNextDose
+                              dict["message"] = "Reminder to take " + medication
+                          } 
+                      } else {
+                          dict["delay"] = -1
+                          dict["message"] = "Reminder to take " + medication
+                      }
+                      
+                  }
+
+              })
+
+              
+
+          } else {
+              dict = {"delay" : -2, "message" : "No ongoing medication regimes"}
+          }
+
+          return dict
+          }
+
+
+          async function showAlert(userId, medication) {
+
+
+            var dict = await setTimedAlert(userId)
+
+            // console.log(dict["delay"])
+
+            setTimeout(() => {
+
+                // this.show = true;
+                if (dict["delay"] == -1) {
+                    // alert("You have yet to take your medications")
+                   
+                } else if (dict["delay"] == -2) {
+                        // no ongoing medicatio
+                } else {
+                    var result = confirm(dict["message"] + ".\nDo you want to record your consumption now?")
+                    if (result) {
+                      window.location.href = "/overview"
+                    } 
+                } 
+            }, dict["delay"]);
+
+          }
+
+          async function readMedication(userId) {
 
             // const db = getFirestore(firebaseApp);
             // const auth = await getAuth(firebaseApp);
@@ -68,166 +173,191 @@ export default {
 
             let index = 1
             let table = document.getElementById("table")
-            
+
             if (!allDocuments.empty) {
 
-                // console.log(allDocuments)
+                  // console.log(allDocuments)
+              console.log(table.rows.length)
+              if (table.rows.length <= 1) {
 
-            allDocuments.forEach((docs) => {
 
-                let row = table.insertRow(index)
+                allDocuments.forEach((docs) => {
 
-                let documentData = docs.data()
-                
-                let medication = (documentData.Medication)
-                let dosage = (documentData.Dosage)
-                let frequency = (documentData.Frequency)
-                let reminders = (documentData.Reminders)    
-                let taken = (documentData.Taken)   
-                let lag = (documentData.Lag)
-                
-                // console.log(taken)
-                
-                var numberOfDosesTaken = taken.length
+                    let row = table.insertRow(index)
 
-                // console.log(numberOfDosesTaken)
-
-                var checkbox = false
-
-                var minTimeBetweenDoses = Number.MAX_SAFE_INTEGER
-
-                var timeFromLastDose = null;
-
-                // setTimeout(function() { alert("Reminder to take: " + medication); }, 1000);
-
-                if (numberOfDosesTaken > 0) {
+                    let documentData = docs.data()
                     
-                    var lastTaken = taken[taken.length - 1].toDate()
+                    let medication = (documentData.Medication)
+                    let dosage = (documentData.Dosage)
+                    let frequency = (documentData.Frequency)
+                    let reminders = (documentData.Reminders)    
+                    let taken = (documentData.Taken)   
+                    let lag = (documentData.Lag)
+                    
+                    // console.log(taken)
+                    
+                    var numberOfDosesTaken = taken.length
 
                     // console.log(numberOfDosesTaken)
-                    var today = new Date();
-                    var timeFromLastDose = (today.getHours() - lastTaken.getHours()) * 60 + (today.getMinutes() - lastTaken.getMinutes()) 
-                    
-                    // minTimeBetweenDoses = Number.MAX_SAFE_INTEGER
 
-                    
-                    var hoursBetweenDoses = parseInt(reminders.split(":")[0])
-                    var minutesBetweenDoses = parseInt(reminders.split(":")[1])
+                    var checkbox = false
 
-                    var timeBetweenDoses = hoursBetweenDoses * 60 + minutesBetweenDoses
+                    var minTimeBetweenDoses = Number.MAX_SAFE_INTEGER
 
-                    
-                    if (timeBetweenDoses < minTimeBetweenDoses) {
-                        minTimeBetweenDoses = timeBetweenDoses
-                    }
+                    var timeFromLastDose = null;
 
+                    // setTimeout(function() { alert("Reminder to take: " + medication); }, 1000);
 
-                    // var checkbox = false
-                    
-                    if (timeFromLastDose < minTimeBetweenDoses) {
-                        checkbox = true
-                    }
-                    
-                }
+                    if (numberOfDosesTaken > 0) {
+                        
+                        var lastTaken = taken[taken.length - 1].toDate()
 
-                let cell0 = row.insertCell(0);
-                let cell1 = row.insertCell(1);
-                let cell2 = row.insertCell(2);
-                let cell3 = row.insertCell(3);
-                let cell4 = row.insertCell(4);
+                        // console.log(numberOfDosesTaken)
+                        var today = new Date();
 
-                let cell5 = row.insertCell(5);
-                let cell6 = row.insertCell(6);
+                        // var timeFromLastDose = (today.getHours() - lastTaken.getHours()) * 60 + (today.getMinutes() - lastTaken.getMinutes()) 
+                        var timeFromLastDose = (today.getTime() - lastTaken.getTime()) / (1000 * 60)
+                        // timeFromLastDose = Math.ceil(timeFromLastDose)
 
-                cell0.innerHTML = index;
-                cell1.innerHTML = medication;
-                cell2.innerHTML = dosage;
-                cell3.innerHTML = frequency;
-                cell4.innerHTML = reminders;
+                        // minTimeBetweenDoses = Number.MAX_SAFE_INTEGER
 
-                let takenButton = document.createElement("button")
-                takenButton.id = String(medication)
-                takenButton.classList.add()
+                        
+                        var hoursBetweenDoses = parseInt(reminders.split(":")[0])
+                        var minutesBetweenDoses = parseInt(reminders.split(":")[1])
 
+                        var timeBetweenDoses = hoursBetweenDoses * 60 + minutesBetweenDoses
 
-                if (checkbox) {
-                    cell5.innerHTML = "Taken"
-
-                } else {
-                    takenButton.innerHTML = "Take Medication Now"
-                    cell5.appendChild(takenButton)
-                    if (taken.length > 0) {
-                        taken.push(Timestamp.fromDate(new Date()))
-                        var lagSeconds = timeFromLastDose - minTimeBetweenDoses
-                        lag.push(lagSeconds)
-                    } else {
-                        taken = new Array(Timestamp.fromDate(new Date()))
-                        lag = new Array()
-                        // lag.push(0)
-                    }
-                    
-                    takenButton.onclick = async function() {
-
-                        let takeMedication = { 
-                            Medication: medication,
-                            Dosage: dosage,
-                            Frequency: frequency,
-                            Reminders: reminders,
-                            Taken: taken,
-                            Lag: lag
+                        
+                        if (timeBetweenDoses < minTimeBetweenDoses) {
+                            minTimeBetweenDoses = timeBetweenDoses
                         }
 
-                      
-                        const docRef = await setDoc(doc(db, "PillPal", userId, "MedicationRegime", medication), takeMedication)
-                        alert("Medication has been taken at: " + new Date())
-                        location.reload()
-                        
 
+                        // var checkbox = false
+                        
+                        if (timeFromLastDose < minTimeBetweenDoses) {
+                            checkbox = true
+                            // console.log(timeFromLastDose, minTimeBetweenDoses)
+                        }
                         
                     }
-                }
-                
-                let deleteButton = document.createElement("button")
-                deleteButton.id = String(medication)
-                deleteButton.className = "deleteButton"
-                deleteButton.innerHTML = "Delete"
 
-                
+                    let cell0 = row.insertCell(0);
+                    let cell1 = row.insertCell(1);
+                    let cell2 = row.insertCell(2);
+                    let cell3 = row.insertCell(3);
+                    let cell4 = row.insertCell(4);
 
-                cell6.appendChild(deleteButton)
-                deleteButton.onclick = function() {
+                    let cell5 = row.insertCell(5);
+                    let cell6 = row.insertCell(6);
 
-                    deleteMedication(medication)
-                    // alert("You are going to delete " + medication)
-                    // await deleteDoc(doc(db,"PillPal", userId, "MedicationRegime", medication))
+                    cell0.innerHTML = index;
+                    cell1.innerHTML = medication;
+                    cell2.innerHTML = dosage;
+                    cell3.innerHTML = frequency;
+                    cell4.innerHTML = reminders;
 
-                    // console.log("Medication " + medication + " successfully deleted!")
+                    let takenButton = document.createElement("button")
+                    takenButton.id = String(medication)
+                    takenButton.classList.add()
 
-                    // let tb = document.getElementById("table")
-                    // while (tb.ariaRowSpan.length > 1) {
-                    //     tb.deleteRow(1)
+
+                    if (checkbox) {
+                        cell5.innerHTML = "Taken"
+
+                    } else {
+                        takenButton.innerHTML = "Take Medication Now"
+                        cell5.appendChild(takenButton)
+                        if (taken.length > 0) {
+                            taken.push(Timestamp.fromDate(new Date()))
+                            var lagMin = timeFromLastDose - minTimeBetweenDoses
+                            lag.push(lagMin)
+                            if (lagMin <= 60) {
+                              updateDoc(doc(db, "PillPal", userId), {
+                                Reward_Points: increment(3),
+                              })
+                            }
+                        } else {
+                            taken = new Array(Timestamp.fromDate(new Date()))
+                            lag = new Array()
+                            updateDoc(doc(db, "PillPal", userId), {
+                                Reward_Points: increment(3),
+                              })
+                            // lag.push(0)
+                        }
+                        
+                        takenButton.onclick = async function() {
+
+                            let takeMedication = { 
+                                Medication: medication,
+                                Dosage: dosage,
+                                Frequency: frequency,
+                                Reminders: reminders,
+                                Taken: taken,
+                                Lag: lag
+                            }
+
+                          
+                            const docRef = await setDoc(doc(db, "PillPal", userId, "MedicationRegime", medication), takeMedication)
+                            alert("Medication has been taken at: " + new Date())
+                            if (reminders != "00:00") {
+                              cell5.innerHTML = "Taken"
+                            }
+                            // location.reload()
+                            showAlert(userId, medication)
+                            
+
+                            
+                        }
+                    }
+                    
+                    let deleteButton = document.createElement("button")
+                    deleteButton.id = String(medication)
+                    deleteButton.className = "deleteButton"
+                    deleteButton.innerHTML = "Delete"
+
+                    
+
+                    cell6.appendChild(deleteButton)
+                    deleteButton.onclick = async function() {
+
+                      // deleteMedication(medication)
+                      alert("You are going to delete " + medication)
+                      await deleteDoc(doc(db,"PillPal", userId, "MedicationRegime", medication))
+
+                      console.log("Medication " + medication + " successfully deleted!")
+
+                      let tb = document.getElementById("table")
+                    
+                      // tb.deleteRow(index)
+                     
+                      while (tb.rows.length > 1) {
+                          tb.deleteRow(1)
+                      }
+                      readMedication(userId)
+                      // location.reload();
+                      
+                    }
+
+                    // var f = '<input type="checkbox" onchange="checkboxToggle" id="complete" value="no">';
+                    
+                    // if (checkbox) {
+                    //     cell5.innerHTML = "Taken";
+                    // } else {
+                    //     cell5.innerHTML = f
                     // }
-                    // readMedication();
-                }
-
-                // var f = '<input type="checkbox" onchange="checkboxToggle" id="complete" value="no">';
-                
-                // if (checkbox) {
-                //     cell5.innerHTML = "Taken";
-                // } else {
-                //     cell5.innerHTML = f
-                // }
-                
+                    
 
 
-                index += 1
-            })
+                    index += 1
+                })
+              }
 
-        }
-        }
+            }
+            }
 
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
+            const auth = getAuth();
+            onAuthStateChanged(auth, (user) => {
             if (user) {
                 readMedication(user.email)
                 // ...
@@ -235,26 +365,28 @@ export default {
                 // User is signed out
                 // ...
             }
-        });
+            });
 
 
-        async function takeMedication(medication) {
-            let takeMedication = { Taken: admin.firestore.TimeStamp.fromDate(new Date()) }
-            const docRef = await setDoc(doc(db, "PillPal", userId, "MedicationRegime", medication), takeMedication)
-        }
 
-        async function deleteMedication(medication) {
-            alert("You are going to delete " + medication)
-            await deleteDoc(doc(db,"PillPal", userId, "MedicationRegime", medication))
 
-            console.log("Medication " + medication + " successfully deleted!")
+        // async function takeMedication(medication) {
+        //     let takeMedication = { Taken: admin.firestore.TimeStamp.fromDate(new Date()) }
+        //     const docRef = await setDoc(doc(db, "PillPal", userId, "MedicationRegime", medication), takeMedication)
+        // }
 
-            let tb = document.getElementById("table")
-            while (tb.rows.length > 1) {
-                tb.deleteRow(1)
-            }
-            readMedication();
-        }
+        // async function deleteMedication(medication, userId) {
+        //     alert("You are going to delete " + medication)
+        //     await deleteDoc(doc(db,"PillPal", userId, "MedicationRegime", medication))
+
+        //     console.log("Medication " + medication + " successfully deleted!")
+
+        //     let tb = document.getElementById("table")
+        //     while (tb.rows.length > 1) {
+        //         tb.deleteRow(1)
+        //     }
+        //     readMedication(userId);
+        // }
     
     // }
         
